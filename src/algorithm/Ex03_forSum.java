52767aec03ebb349eb8568f18bbf7a9d80272dc8
anjaemun <ajm0111@naver.com>
2023-12-20 17:16:59 +0900

    백준 알고리즘 - for문 Sum

---
+++ src/algorithm/Ex03_forSum.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Ex03_forSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
